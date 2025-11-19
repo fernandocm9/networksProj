e15b37f143dedd57acd41e3a25125a6c1358e8f7
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2025-11-19 15:46:27 -0500

    corrected references to sendObj and recObj function that were imported and changed

--- ChatClient.py
+++ ChatClient.py
@@ -12,7 +12,7 @@ white_font = Fore.WHITE
 import socket#brings in the socket module so that the client can communicate over the network
 import threading#threading module will allow for multiple tasks to be run at once while over the same process
 from email.mime.text import MIMEText#formatting for the text to be used with messages being sent and received
-from protocol import send_object, recv_object#gets these modules from protocol.py for sending and receiving messages
+from protocol import sendObject, receiveObject#gets these modules from protocol.py for sending and receiving messages
 #start the ChatClient  
 class ChatClient:
     def __init__(self):
@@ -35,7 +35,7 @@ class ChatClient:
     def listen_loop(self):
         while True:#while the thread is looping to listen for incoming messages
             try:
-                msg = recv_object(self.sock)#receives an object from the server over the network socket
+                msg = receiveObject(self.sock)#receives an object from the server over the network socket
                 if msg is None:#if there is no message received, the server has disconnected
                     print(red_font + "The server has disconnected.")
                     self.sock.close()#close the socket connection
@@ -72,13 +72,13 @@ class ChatClient:
                     print(yellow_font + "Use this format: /nick <nickname>")#provides user with correct format for the command
                     continue#continues to prompt the user for input
                 self.nickname = parts[1]#retrieves the nickname from the input
-                send_object(self.sock, {"type": "command", "command": "nick", "args": [self.nickname]})#sends the nickname to the server as a command object to set the user's nickname
+                sendObject(self.sock, {"type": "command", "command": "nick", "args": [self.nickname]})#sends the nickname to the server as a command object to set the user's nickname
 
             elif text.startswith("/list"):#for when the user inputs the /list command to request a list of available channels and the number of users in each channel
                 if self.sock is None:#for when there is no connection to a server yet
                     print(yellow_font + "To see a list of available channels and the # of users for each channel, you must /connect to a server first.")#lets the user know that they must connect to a server before requesting a list
                     continue#continues to prompt the user for input
-                send_object(self.sock, {"type": "command", "command": "list"})#sends the list command to the server as a command object to request the list of channels and # of users per channel
+                sendObject(self.sock, {"type": "command", "command": "list"})#sends the list command to the server as a command object to request the list of channels and # of users per channel
 
             elif text.startswith("/join"):#for when the user inputs the /join command to join a specific channel
                 if self.sock is None:#for when there is no connection to a server yet
@@ -89,7 +89,7 @@ class ChatClient:
                     print(yellow_font + "Use this format: /join <channel>")#provides user with correct format for the command
                     continue#continues to prompt the user for input
                 channel = parts[1]#retrieves the channel name from the input
-                send_object(self.sock, {"type": "command", "command": "join", "args": [channel]})#sends the join command to the server as a command object to join the specified channel
+                sendObject(self.sock, {"type": "command", "command": "join", "args": [channel]})#sends the join command to the server as a command object to join the specified channel
                 
             elif text.startswith("/leave"):#for when the user inputs the /leave command to leave a channel
                 if self.sock is None:#for when there is no connection to a server yet
@@ -97,12 +97,12 @@ class ChatClient:
                     continue#continues to prompt the user for input
                 parts = text.split(maxsplit=1)#splits the input text into parts based on whitespace, command entered and channel name
                 args = [parts[1]] if len(parts) > 1 else []#retrieves the channel name if provided
-                send_object(self.sock, {"type": "command", "command": "leave", "args": args})#sends the leave command to the server as a command object to leave the specified channel
+                sendObject(self.sock, {"type": "command", "command": "leave", "args": args})#sends the leave command to the server as a command object to leave the specified channel
 
             elif text.startswith("/quit"):#for when the user inputs the /quit command to disconnect from the server and exit the client
-                send_object(self.sock, {"type": "command", "command": "quit"})#sends the quit command to the server as a command object to disconnect from the server and leave the chat
+                sendObject(self.sock, {"type": "command", "command": "quit"})#sends the quit command to the server as a command object to disconnect from the server and leave the chat
                 try:#attempts to receive any final messages from the server before disconnecting
-                    obj = recv_object(self.sock)#receives an object from the server over the network socket
+                    obj = receiveObject(self.sock)#receives an object from the server over the network socket
                     if obj:#if there is a message received from the server
                         print(obj.get("info", obj) + "\n")#prints the received message to the console for the user to see
                 except:#for when there is an error receiving the message
